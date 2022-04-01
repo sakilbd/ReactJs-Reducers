6f66eb026cdabef23aa378b39f609a30e5860e91
unknown <mdsakiluzzaman@gmail.com>
2022-04-01 21:31:27 +0600

    project setup

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './index.css';
+
+
+
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  
+render(){ 
+return (
+  <>
+  <h1>Hellow World</h1>
+  </>
+  );
+}
+}
+
+export default App;
